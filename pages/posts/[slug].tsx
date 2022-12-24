df8ba47e0b9ec7289766e177032320bcfff43839
Brandon Kase <brandon.kase@gmail.com>
2022-12-24 14:27:11 -0500

    Initial commit

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Head from "next/head"
+import { allPosts, Post } from "contentlayer/generated"
+import { useMDXComponent } from "next-contentlayer/hooks"
+import { CH } from "@code-hike/mdx/components"
+
+export async function getStaticPaths() {
+  const paths: string[] = allPosts.map((post) => post.url)
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+const PostLayout = ({ post }: { post: Post }) => {
+  const MDXContent = useMDXComponent(post.body.code)
+  return (
+    <article style={{ width: 600, margin: "0 auto" }}>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+      <h1>{post.title}</h1>
+      <MDXContent components={{CH : CH}} />
+    </article>
+  )
+}
+
+export default PostLayout
